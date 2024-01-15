0d59e69821499329a22510cd41551d7f4797bdf9
ulfatjon <xamrokuloffulfatjon@gmail.com>
2024-01-15 13:42:51 +0500

    added to js code

---
+++ app.js
@@ -0,0 +1,39 @@
+//1 - Masala.
+
+// let username=prompt("ismingizni kiriting!")
+// let age=prompt("yoshingiz nechada!")*1;
+// let street=prompt("qaysi ko'chada turasiz!");
+// let city=prompt("shaharingiz!");
+// let zipcode=prompt("zip kodni kiriting!");
+// const address={
+//     username:username,
+//     age:age,
+//     city:city,
+//     street:street,
+//     zipcode:zipcode,
+// }
+// function showAddress(){
+//     console.log(address);
+// }
+// showAddress()
+
+//2 - Masala.
+
+// function old35(n){
+//     if(n%3==0 && n%5==0){
+//         console.log(false);
+//     }
+//     else if( n%3==0){
+//         console.log(true);
+//     }
+//     else if(n%5==0){
+//         console.log(true);
+//     }
+//     else if(n%3==1 || n%5==1){
+//         console.log(false);
+//     }
+//     else{
+//         console.log("Not And Number");
+//     }
+// }
+// old35(9)
